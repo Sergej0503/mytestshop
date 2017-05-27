3198662e00f688ece308676545dab751151f6cc5
Sergej Streck <sergej_streck@mail.ru>
2017-05-27 09:00:49 +0200

    First commit

---
+++ landing/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .forms import SubscriberForm
+from products.models import *
+
+
+def landing(request):
+    # name = "Сергей"
+    # current_day = "19.05.2017"
+    form = SubscriberForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        # name = form.cleaned_data['name']
+        # print(name)
+        new_form = form.save()
+
+    return render(request, 'landing/landing.html', locals())
+
+
+def home(request):
+    products_images = ProductImage.objects.filter(is_active=True, is_main=True)
+    return render(request, 'landing/home.html', locals())
